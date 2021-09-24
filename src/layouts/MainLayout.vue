7c2322fbf613c80f086273ab688aaab8f61d3926
Gilberto Rempto <gilberto.rempto@gmail.com>
2021-09-24 10:00:03 -0300

    sidebar component

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,17 +2,7 @@
   <q-layout view="lHh Lpr lFf">
     <w-header> </w-header>
 
-    <q-drawer v-model="sidebarOpen" show-if-above bordered>
-      <q-list>
-        <q-item-label header> Essential Links </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
+    <w-sidebar></w-sidebar>
 
     <q-page-container>
       <router-view />
@@ -21,74 +11,18 @@
 </template>
 
 <script>
-import EssentialLink from "components/EssentialLink.vue";
-
-const linksList = [
-  {
-    title: "Docs",
-    caption: "quasar.dev",
-    icon: "school",
-    link: "https://quasar.dev",
-  },
-  {
-    title: "Github",
-    caption: "github.com/quasarframework",
-    icon: "code",
-    link: "https://github.com/quasarframework",
-  },
-  {
-    title: "Discord Chat Channel",
-    caption: "chat.quasar.dev",
-    icon: "chat",
-    link: "https://chat.quasar.dev",
-  },
-  {
-    title: "Forum",
-    caption: "forum.quasar.dev",
-    icon: "record_voice_over",
-    link: "https://forum.quasar.dev",
-  },
-  {
-    title: "Twitter",
-    caption: "@quasarframework",
-    icon: "rss_feed",
-    link: "https://twitter.quasar.dev",
-  },
-  {
-    title: "Facebook",
-    caption: "@QuasarFramework",
-    icon: "public",
-    link: "https://facebook.quasar.dev",
-  },
-  {
-    title: "Quasar Awesome",
-    caption: "Community Quasar projects",
-    icon: "favorite",
-    link: "https://awesome.quasar.dev",
-  },
-];
-
 import { defineComponent, computed } from "vue";
-import { useStore } from "vuex";
 import WHeader from "components/WHeader.vue";
+import WSidebar from "src/components/WSidebar.vue";
 
 export default defineComponent({
+  name: "MainLayout",
+
   components: {
     WHeader,
-    EssentialLink,
+    WSidebar,
   },
-  setup() {
-    const $store = useStore();
 
-    const sidebarOpen = computed({
-      get: () => $store.state.layout.sidebarOpen,
-      set: (value) => $store.commit("layout/setSidebarOpen", value),
-    });
-
-    return {
-      essentialLinks: linksList,
-      sidebarOpen,
-    };
-  },
+  setup() {},
 });
 </script>
