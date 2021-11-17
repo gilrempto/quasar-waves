59607dce46609d0b003a2b0f15c575b4fd1a3fca
Gil Rempto <gilrempto@gmail.com>
2021-11-17 09:25:52 -0300

    Forms page toolbar

--- src/pages/Forms.vue
+++ src/pages/Forms.vue
@@ -2,16 +2,38 @@
   <w-page>
     <h1>Forms</h1>
 
-    <div class="q-gutter-md">
-      <w-form-login></w-form-login>
+    <h2 id="login">Login</h2>
+    <w-form-login></w-form-login>
 
-      <w-form-register></w-form-register>
+    <h2 id="register">Register</h2>
+    <w-form-register></w-form-register>
 
-      <w-form-forget-password></w-form-forget-password>
-    </div>
+    <h2 id="forget-password">Forget Password</h2>
+    <w-form-forget-password></w-form-forget-password>
   </w-page>
 
   <q-drawer v-model="toolbarOpen" show-if-above bordered side="right">
+    <q-scroll-area class="fit">
+      <q-list>
+        <q-item to="#login" clickable v-ripple>
+          <q-item-section>
+            <q-item-label>Login</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item to="#register" clickable v-ripple>
+          <q-item-section>
+            <q-item-label>Register</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item to="#forget-password" clickable v-ripple>
+          <q-item-section>
+            <q-item-label>Forget Password</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-scroll-area>
   </q-drawer>
 </template>
 
